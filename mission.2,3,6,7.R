f18b8dd08fa604c8ea93c86a1ec5c0159c53d0c9
nsavi <112178048+nsavi@users.noreply.github.com>
2023-06-07 00:19:33 -0700

    Update mission.2,3,4,6,7,8,9.R

--- mission.2,3,6,7.R
+++ mission.2,3,6,7.R
@@ -203,7 +203,45 @@ zalpha
 if(z<=zalpha){print("Accept Null hypothesis")} else{print("Reject Null 
 hypothesis")}
 -----------------------------------------------------------------------------------------------------------
+Exp-9
+> #Two indep sample sizes of size 8 and 8 contained the following values
+> #sample 1 - 19 - 17 - 15 - 21 - 16 - 18 - 16 - 14
+> #sample 2 - 15 - 14 - 15 - 19 - 15 - 18 - 16 - 20
+> #Is the difference btw sample means significant ?
+> #input the data
+> sample1 = c(19,17,15,21,16,18,16,14)
+> sample2 = c(15,14,15,19,15,18,16,20)
+> sample1
+[1] 19 17 15 21 16 18 16 14
+> sample2
+[1] 15 14 15 19 15 18 16 20
+> #output using t-distribution
+> t=t.test(sample1,sample2)
+> t
 
+	Welch Two Sample t-test
+
+data:  sample1 and sample2
+t = 0.44721, df = 13.989, p-value = 0.6616
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -1.898128  2.898128
+sample estimates:
+mean of x mean of y 
+     17.0      16.5 
+
+> #test-statistics
+> cv = t$statistic
+> cv
+        t 
+0.4472136 
+> #Critical value
+> tv = qt(0.975,14)
+> tv
+[1] 2.144787
+> #conclusion
+> if(cv<=tv){print("Accept H0")} else{print("Reject H0")}
+[1] "Accept H0"
 
 
 
